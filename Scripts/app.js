e5bb0a2386e1c474b0bd5046e7dab4ef12ff43de
Ryan Riley <ryan@tachyus.com>
2014-06-04 11:41:59 -0500

    Switch to Angular-UI-Router for navigation.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,19 +1,39 @@
 ﻿; (function (factory) {
-    'use strict';
+    "use strict";
 
     var root = this;
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports', 'document', 'navigator', 'jQuery'], factory);
+        define(['exports', 'document', 'navigator', 'jQuery', 'jstz', 'angular'], factory);
     } else if (typeof exports === 'object') {
         // CommonJS
-        factory(exports, require('document'), require('navigator'), require('jQuery'));
+        factory(exports, require('document'), require('navigator'), require('jQuery'), require('jstz'), require('angular'));
     } else {
         // Browser globals
-        factory((root.commonJsStrict = {}), root.document, root.navigator, root.jQuery);
+        factory((root.commonJsStrict = {}), root.document, root.navigator, root.jQuery, root.jstz, root.angular);
     }
-}.call(this, function (window, document, navigator, $, undefined) {
+}.call(this, function (window, document, navigator, $, jstz, angular, undefined) {
     'use strict';
+
+    var timezone = jstz.determine(),
+    	tzname = timezone.name();
+
+    $(document).ready(function () {
+        var calurl = "https://www.google.com/calendar/embed?showTitle=0&showPrint=0&showCalendars=0&mode=AGENDA&height=600&wkst=1&bgcolor=%23FFFFFF&src=n1bhbfnd2uvh807a2vr7s0ar1g@group.calendar.google.com&color=%23711616&ctz=" + tzname;
+        $('#agenda_frame').attr('src', calurl);
+    });
+
+    var app = angular.module('c4fsharp', ['ui.router']);
+
+    app.config(
+        ['$stateProvider',
+         '$urlRouterProvider',
+         function ($stateProvider, $urlRouterProvider) {
+             $stateProvider.state('main',   { url: '/',   templateUrl: 'main.html' });
+             $stateProvider.state('groups', { url: '/groups', templateUrl: 'groups.html' });
+             $stateProvider.state('events', { url: '/events', templateUrl: 'events.html' });
+             $urlRouterProvider.otherwise('/');
+        }]);
 
     /*!
      * Copyright 2013 Twitter, Inc.
